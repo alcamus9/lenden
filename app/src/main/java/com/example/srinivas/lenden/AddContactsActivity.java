077890354ce362da13aacfc8f1fec609eaaafd00
unknown <ssunkara@tepper.cmu.edu>
2016-03-21 01:22:19 -0400

    color edits

--- app/src/main/java/com/example/srinivas/lenden/AddContactsActivity.java
+++ app/src/main/java/com/example/srinivas/lenden/AddContactsActivity.java
@@ -3,6 +3,7 @@ package com.example.srinivas.lenden;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -36,6 +37,9 @@ public class AddContactsActivity extends AppCompatActivity implements  AdapterVi
         for (int i=0; i < data.size(); i++) {
             this.dataNames.add(data.get(i).getName());
         }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         autoCompleteAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_activated_1, this.dataNames);
         contactSearchView.setAdapter(autoCompleteAdapter);
